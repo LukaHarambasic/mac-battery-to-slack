48d5b0025dc698ca60b4a753dd0249289faf2039
Luka Harambasic <luka@harambasic.de>
2022-11-15 21:15:40 +0100

    init2

---
+++ README.md
@@ -0,0 +1,18 @@
+# Personal Battery Bot
+
+TBD
+
+## Slack
+
+* Block Kit Builder
+    * https://app.slack.com/block-kit-builder/T02CB9DN17T
+* Slack API
+    * Create Bot with webhook
+        * https://api.slack.com/apps/A04AMB5JZGF/incoming-webhooks?success=1
+
+
+## Run it
+
+* https://betterprogramming.pub/https-medium-com-ratik96-scheduling-jobs-with-crontab-on-macos-add5a8b26c30
+* open cron via nano `export VISUAL=nano; crontab -e`
+* `*/5 * * * * cd ~/Development/battery-bot ./battery-bot.sh YOUR-WEBHOOK`
---
+++ battery-bot.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+MESSAGES=("Pleeeassseeeee charge me!" "You know what I'm going to tell you..." "Come on, again?" "You are looking sharp today, but have you thought about charging me?" "Want to embarrass yourself during a meeting again?" "Should Luka get you a charger? Again?" " I won't say anything.")
+BATTERY_LEVEL=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
+
+if (($BATTERY_LEVEL < 65))
+then
+    MESSAGE_MAIN=${MESSAGES[RANDOM%${#MESSAGES[@]}]}
+    MESSAGE_LEVEL=":low_battery: ${BATTERY_LEVEL}%"
+
+    DATA_RAW='{ "blocks": [ { "type": "section", "text": { "type": "mrkdwn", "text": "%s" } }, { "type": "context", "elements": [ { "type": "plain_text", "text": "%s", "emoji": true } ] } ] }'
+    DATA=$(printf "$DATA_RAW" "$MESSAGE_MAIN" "$MESSAGE_LEVEL")
+
+    curl -X POST -H 'Content-type: application/json' --data "${DATA}" $1
+
+    echo "It is done!"
+else
+    echo "Not now!"
+fi
+
+
